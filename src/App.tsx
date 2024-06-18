8940adc3a5a617bb25210c6be17c65458b1b2569
lucashenrique1011 <150748871+lucashenrique1011@users.noreply.github.com>
2024-06-18 15:41:45 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Topo from './topo'
+import './App.css'
+import '../fontea/css/all.min.css'
+
+function App() {
+  return (
+    <>
+    
+        <div className="topo "><Topo /></div>
+      
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/topo.tsx
@@ -0,0 +1,13 @@
+import './topo.css'
+
+function Topo() {
+    return (
+        <>
+            <div className='topof'>
+                <h1>studs</h1>
+            </div>
+        </>
+    )
+}
+
+export default Topo
